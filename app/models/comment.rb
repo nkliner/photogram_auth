1b07fa543f4087177b46b1eb726656b4db120c8f
Nick Kliner <nkliner@chicagobooth.edu>
2017-05-29 14:25:44 -0500

    After associations and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
+
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
+
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+
+  validates :user_id, :presence => true
+
+  validates :photo_id, :presence => true
+
+  validates :body, :presence => true
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,11 @@
 class Like < ApplicationRecord
+
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
+
+  validates :user_id, :presence => true, :uniqueness => { :scope => :photo }
+
+  validates :photo_id, :presence => true
+
 end
