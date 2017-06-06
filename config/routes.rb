2d7f6cd193622e81d17be3761fad96f7db3e4889
Nick Kliner <nkliner@chicagobooth.edu>
2017-06-05 21:46:25 -0500

    RCAV user changes

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,8 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  get "/users", :controller => "users", :action => "index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
