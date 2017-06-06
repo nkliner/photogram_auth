e33b6d20adea2f493fe12890120d6228e5a2e697
Nick Kliner <nkliner@chicagobooth.edu>
2017-06-05 22:08:18 -0500

    Current user

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/users", :controller => "users", :action => "index"
-
+  get "/users/:id", :controller => "users", :action => "show"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
